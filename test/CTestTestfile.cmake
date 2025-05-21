72ffa67db1f870f3ae9f36abeda981e3ed8ec94c
Mr.shrinivas <83639255+mrshri07@users.noreply.github.com>
2025-05-21 11:36:07 +0530

    Delete test/CTestTestfile.cmake
    no need

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/058yr/google_Test/test
-# Build directory: /home/058yr/google_Test/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ExampleTests "/home/058yr/google_Test/test/ExampleTests.exe")
-set_tests_properties(ExampleTests PROPERTIES  _BACKTRACE_TRIPLES "/home/058yr/google_Test/test/CMakeLists.txt;15;add_test;/home/058yr/google_Test/test/CMakeLists.txt;0;")
